6d2632b403e8b603e36395a51e3172b37aed2520
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2021-07-15 15:40:16 +1000

    Update for i2c-options unified library

--- PiicoDev_VEML6030.py
+++ PiicoDev_VEML6030.py
@@ -2,7 +2,6 @@
 # Written by Michael Ruppe at Core Electronics MAR 2021
 
 from PiicoDev_Unified import *
-i2c = PiicoDev_Unified_I2C()
 
 # Registers
 _veml6030Address = 0x10
@@ -12,8 +11,8 @@ _REG_ALS = b'\x04'
 _DEFAULT_SETTINGS = b'\x00' # initialise gain:1x, integration 100ms, persistence 1, disable interrupt
 
 class PiicoDev_VEML6030(object):    
-    def __init__(self, addr=_veml6030Address, i2c=i2c):
-        self.i2c = i2c
+    def __init__(self, bus=None, freq=None, sda=None, scl=None, addr=_veml6030Address):
+        self.i2c = create_unified_i2c(bus=bus, freq=freq, sda=sda, scl=scl)
         self.addr = addr
         self.res = 0.0288 # lx/bit
         self.i2c.write8(self.addr, _ALS_CONF, _DEFAULT_SETTINGS)
