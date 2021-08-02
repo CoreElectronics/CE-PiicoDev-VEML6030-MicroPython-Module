b375094aa2f46fabbbca0dc8d2a1952a61b14d4b
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-08-02 13:56:47 +1000

    Works with LoPy, use readfrom_mem/writeto_mem

--- PiicoDev_VEML6030.py
+++ PiicoDev_VEML6030.py
@@ -6,19 +6,18 @@ i2c = PiicoDev_Unified_I2C()
 
 # Registers
 _veml6030Address = 0x10
-_ALS_CONF = b'\x00'
-_REG_ALS = b'\x04'
+_ALS_CONF = 0x00
+_REG_ALS = 0x04
 
-_DEFAULT_SETTINGS = b'\x00' # initialise gain:1x, integration 100ms, persistence 1, disable interrupt
+_DEFAULT_SETTINGS = 0x00 # initialise gain:1x, integration 100ms, persistence 1, disable interrupt
 
 class PiicoDev_VEML6030(object):    
     def __init__(self, addr=_veml6030Address, i2c=i2c):
         self.i2c = i2c
         self.addr = addr
         self.res = 0.0288 # lx/bit
-        self.i2c.write8(self.addr, _ALS_CONF, _DEFAULT_SETTINGS)
+        self.i2c.writeto_mem(self.addr, _ALS_CONF, _DEFAULT_SETTINGS)
         
     def read(self):
-        data = self.i2c.read16(self.addr, _REG_ALS)
+        data = self.i2c.readfrom_mem(self.addr, _REG_ALS, 2)
         return int.from_bytes(data, 'little') * self.res
-          
